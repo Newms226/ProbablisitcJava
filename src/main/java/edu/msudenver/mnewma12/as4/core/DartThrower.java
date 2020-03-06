124c34922596060e90845548b4f36dde5cfa5457
MichaelNewman <newms226@gmail.com>
2020-03-06 10:56:57 -0700

    init + Point spec

---
+++ src/main/java/edu/msudenver/mnewma12/as4/core/DartThrower.java
@@ -0,0 +1,9 @@
+package edu.msudenver.mnewma12.as4.core;
+
+public class DartThrower {
+
+    void throwDart() {}
+
+    boolean isInside() { return false; }
+
+}
---
+++ src/main/java/edu/msudenver/mnewma12/as4/core/Runner.java
@@ -0,0 +1,20 @@
+package edu.msudenver.mnewma12.as4.core;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void calculatePi() {}
+
+    public static void testPrimes() {}
+
+    public static void randomArray() {}
+
+    public static void monteCarlo() {}
+
+    public static void eightQueens() {}
+
+
+}
